1940d8f8bc71e962d2c417b9fa9d1e95b0bbe17e
lesenelir <miaomiaobabyzy@gmail.com>
2022-02-25 23:16:36 +0800

    feature: update vue3,0 store

--- src/views/Btn.vue
+++ src/views/Btn.vue
@@ -6,17 +6,36 @@
 </template>
 
 <script>
+import {useStore} from 'vuex'
+
 export default {
   name: 'Btn',
-  methods: {
-    addFn () {
-      // 使用$tore.commit调用store中的mutation中的increase方法
-      // 传参的话 使用payload
-      this.$store.commit('increase', 2)
-    },
-    reduceFn () {
-      // 使用$store.dispatch调用store中的action中的异步方法
-      this.$store.dispatch('decreaseAsync')
+  // methods: {
+  //   addFn () {
+  //     // 使用$tore.commit调用store中的mutation中的increase方法
+  //     // 传参的话 使用payload
+  //     this.$store.commit('increase', 2)
+  //   },
+  //   reduceFn () {
+  //     // 使用$store.dispatch调用store中的action中的异步方法
+  //     this.$store.dispatch('decreaseAsync')
+  //   }
+  // },
+  // Vue3.0 使用useStore 方法
+  setup() {
+    const store = useStore()
+
+    const addFn = () => {
+      store.commit('increase', 2)
+    }
+
+    const reduceFn = () => {
+      store.dispatch('decreaseAsync')
+    }
+
+    return {
+      addFn,
+      reduceFn
     }
   }
 }
